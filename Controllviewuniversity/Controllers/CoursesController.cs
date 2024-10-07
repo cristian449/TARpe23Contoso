bc89259dd450a4a042c5c5306284a85bf68a023e
unknown <Cristianpent12@gmail.com>
2024-10-07 15:41:39 +0300

    Added View method into controller and added view for details

--- Controllviewuniversity/Controllers/CoursesController.cs
+++ Controllviewuniversity/Controllers/CoursesController.cs
@@ -22,6 +22,63 @@ namespace ContosoUniversity.Controllers
             var schoolContext = _context.Courses;
             return View(await schoolContext.ToListAsync());
         }
+
+
+        public async Task<IActionResult> ViewDetails(int? id)
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.FirstOrDefaultAsync(M => M.CourseID == id); 
+
+            if (course == null) 
+            {
+                return NotFound();
+            }
+
+            return View(course);
+
+        }
+
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+
+        [HttpPost, ActionName("ViewDetails")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ViewDetailsPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.CourseID == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View("Details", course);
+        }
     }
 }
 
