7582128cbe84a1f5ec9b8f2a08888fc420be87ac
unknown <Cristianpent12@gmail.com>
2024-10-10 11:42:48 +0300

    Pointless Commit for Deletedetails

--- Controllviewuniversity/Controllers/CoursesController.cs
+++ Controllviewuniversity/Controllers/CoursesController.cs
@@ -24,52 +24,5 @@ namespace ContosoUniversity.Controllers
         }
 
 
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var course = await _context.Courses
-                .FirstOrDefaultAsync(m => m.CourseID == id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            ViewData["Action"] = "Details";
-            return View("DetailsDelete", course); 
-        }
-
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var course = await _context.Courses
-                .FirstOrDefaultAsync(m => m.CourseID == id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            ViewData["Action"] = "Delete";
-            return View("DetailsDelete", course);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var course = await _context.Courses.FindAsync(id);
-            _context.Courses.Remove(course);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-
     }
 }
