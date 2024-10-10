9066e699c3ee1c3c086739a04da04f8299e96273
unknown <Cristianpent12@gmail.com>
2024-10-10 13:29:57 +0300

    Finally after countless retries and nonstop failure i managed, to create a shared view for DetailsDelete and also added Delete and Details method, so happy pointless VERSION

--- Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ Controllviewuniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3bbb228d10bf7a90f7b940ba350a32689788b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d43d6f9d1c09283a1696e420a2f9da946c46574")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
